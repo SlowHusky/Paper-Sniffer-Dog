c11036f39be3f2cebba92baeec05269c643cd1ed
SlowHusky <slowhusky@gmail.com>
2022-03-21 14:37:49 -0300

    Alteração da SECRET_KEY para ser randômica

--- papersnifferdog/papersnifferdog/settings.py
+++ papersnifferdog/papersnifferdog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v6rzcyq1wk=$#99$b@&ow%7+3$@)-5)edkdjx)()4es6&8t-k$'
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(128))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
